be2b2b73dcbbc4e7f954a104a2f10f50a09e5188
Mr Smith <brandoncontu45@gmail.com>
2023-10-22 00:06:42 -0500

    create login and nav-bar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DetailCardComponent } from 'src/app/detail-card/detail-card.component'
+import { LoginComponent } from 'src/app/login/login.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'card', component: DetailCardComponent},
+  {path: 'login', component: LoginComponent},
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
